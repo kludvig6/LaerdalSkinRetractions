c354657d8dd73942458a3e9cd3518c88d0694f8b
Ludvig <ludvig98@lyse.net>
2022-07-20 10:53:31 +0200

    Added second mode for slow breath

--- concurrency_framework.py
+++ concurrency_framework.py
@@ -1,4 +1,8 @@
-""" Based on the 1s-pulse.py. See this program for explaination of serial functions. """
+"""
+Based on the 1s-pulse.py. See python-EK-M-015-main/1s-pulse.py for explaination of serial functions.
+To use this with an Arduino Uno, you must first upload the StandardFirmata sketch from Arduino.
+This can be found in File -> Examples -> Firmata.
+"""
 
 import threading
 import time
@@ -7,6 +11,8 @@ import time
 from playsound import playsound
 from pyfirmata import Arduino, util
 
+rapid_breath = True
+
 board = Arduino("COM9")
 
 iterator = util.Iterator(board)
@@ -14,102 +20,136 @@ iterator.start()
 
 valve_pin7 = board.get_pin("d:7:o")
 valve_pin8 = board.get_pin("d:8:o")
+motor_pin2 = board.get_pin("d:2:o")
+motor_pin3 = board.get_pin("d:3:o")
 
-serial_port = serial.Serial(port="COM8",
+ventus_port = serial.Serial(port="COM8",
                             baudrate=115200,
                             bytesize=8,
                             timeout=2,
                             stopbits=serial.STOPBITS_ONE)
-serial_port.write(b"#W2,0\n")
-serial_port.write(b"#W0,0\n")
-serial_port.write(b"#W10,0\n")
-serial_port.write(b"#W11,0\n")
-serial_port.write(b"#W23,1000\n")
+ventus_port.write(b"#W2,0\n")
+ventus_port.write(b"#W0,0\n")
+ventus_port.write(b"#W10,0\n")
+ventus_port.write(b"#W11,0\n")
 
-def write_valve(valve, value):
-    if valve == 8:
+if rapid_breath:
+    ventus_port.write(b"#W23,1000\n")
+else:
+    ventus_port.write(b"#W23,100\n")
+
+def digitalWrite(pin, value):
+    if pin == 8:
         valve_pin8.write(value)
-    else:
+    elif pin == 7:
         valve_pin7.write(value)
+    elif pin == 3:
+        motor_pin3.write(value)
+    elif pin == 2:
+        motor_pin2.write(value)
 
 def write_pump(value):
     if value == 1:
-        serial_port.write(b"#W0,1\n")
+        ventus_port.write(b"#W0,1\n")
     else:
-        serial_port.write(b"#W0,0\n")
+        ventus_port.write(b"#W0,0\n")
 
 def play_sound(filepath):
     playsound(filepath)
 
-rapid_breath = True
-
 while True:
     if rapid_breath:
-        breath_path = "C:\\Users\\NOLEL1\\Documents\\ttpventus_python_library\\breathing_sounds\\08INNpust.mp3"
-        valve_8_inhale = threading.Thread(target=write_valve,args=(8,1,))
-        valve_7_inhale = threading.Thread(target=write_valve,args=(7,1,))
+        breath_path = "C:\\Users\\NOLEL1\\Documents\\LaerdalSkinRetractions\\breathing_sounds\\08INNpust.mp3"
+        #valve_8_inhale = threading.Thread(target=digitalWrite,args=(8,1,))
+        valve_7_inhale = threading.Thread(target=digitalWrite,args=(7,1,))
+        #motor_3_inhale = threading.Thread(target=digitalWrite,args=(3,1))
+        #motor_2_inhale = threading.Thread(target=digitalWrite,args=(2,0))
         pump_inhale = threading.Thread(target=write_pump,args=(1,))
         sound_inhale = threading.Thread(target=play_sound,args=(breath_path,))
 
-        valve_8_inhale.start()
+        #valve_8_inhale.start()
         valve_7_inhale.start()
+        #motor_3_inhale.start()
+        #motor_2_inhale.start()
         pump_inhale.start()
         sound_inhale.start()
 
-        time.sleep(1)
-        valve_8_inhale.join()
+        time.sleep(1.1)
+        #valve_8_inhale.join()
         valve_7_inhale.join()
+        #motor_3_inhale.join()
+        #motor_2_inhale.join()
         pump_inhale.join()
         sound_inhale.join()
 
-        breath_path = "C:\\Users\\NOLEL1\\Documents\\ttpventus_python_library\\breathing_sounds\\08UTpust.mp3"
-        valve_8_exhale = threading.Thread(target=write_valve,args=(8,0,))
-        valve_7_exhale = threading.Thread(target=write_valve,args=(7,0,))
+        breath_path = "C:\\Users\\NOLEL1\\Documents\\LaerdalSkinRetractions\\breathing_sounds\\08UTpust.mp3"
+        
+        valve_8_exhale = threading.Thread(target=digitalWrite,args=(8,0,))
+        valve_7_exhale = threading.Thread(target=digitalWrite,args=(7,0,))
+        #motor_3_exhale = threading.Thread(target=digitalWrite,args=(3,0))
+        #motor_2_exhale = threading.Thread(target=digitalWrite,args=(2,1))
         pump_exhale = threading.Thread(target=write_pump,args=(0,))
         sound_exhale = threading.Thread(target=play_sound,args=(breath_path,))
 
-        valve_8_exhale.start()
+        #valve_8_exhale.start()
         valve_7_exhale.start()
+        #motor_3_exhale.start()
+        #motor_2_exhale.start()
         pump_exhale.start()
         sound_exhale.start()
 
-        time.sleep(1)
-        valve_8_exhale.join()
+        time.sleep(0.9)
+        #valve_8_exhale.join()
         valve_7_exhale.join()
+        #motor_3_exhale.join()
+        #motor_2_exhale.join()
         pump_exhale.join()
         sound_exhale.join()
 
     else:
-        breath_path = "C:\\Users\\NOLEL1\\Documents\\ttpventus_python_library\\breathing_sounds\\inpust.mp3"
-        valve_8_inhale = threading.Thread(target=write_valve,args=(8,1,))
-        valve_7_inhale = threading.Thread(target=write_valve,args=(7,1,))
+        breath_path = "C:\\Users\\NOLEL1\\Documents\\LaerdalSkinRetractions\\breathing_sounds\\12INNpust.mp3"
+        #valve_8_inhale = threading.Thread(target=digitalWrite,args=(8,1,))
+        valve_7_inhale = threading.Thread(target=digitalWrite,args=(7,1,))
+        #motor_3_inhale = threading.Thread(target=digitalWrite,args=(3,1))
+        #motor_2_inhale = threading.Thread(target=digitalWrite,args=(2,0))
         pump_inhale = threading.Thread(target=write_pump,args=(1,))
         sound_inhale = threading.Thread(target=play_sound,args=(breath_path,))
 
-        valve_8_inhale.start()
+        #valve_8_inhale.start()
         valve_7_inhale.start()
+        #motor_3_inhale.start()
+        #motor_2_inhale.start()
         pump_inhale.start()
         sound_inhale.start()
 
-        time.sleep(1)
-        valve_8_inhale.join()
+        time.sleep(2)
+        #valve_8_inhale.join()
         valve_7_inhale.join()
+        #motor_3_inhale.join()
+        #motor_2_inhale.join()
         pump_inhale.join()
         sound_inhale.join()
 
-        breath_path = "C:\\Users\\NOLEL1\\Documents\\ttpventus_python_library\\breathing_sounds\\utpust.mp3"
-        valve_8_exhale = threading.Thread(target=write_valve,args=(8,0,))
-        valve_7_exhale = threading.Thread(target=write_valve,args=(7,0,))
+        breath_path = "C:\\Users\\NOLEL1\\Documents\\LaerdalSkinRetractions\\breathing_sounds\\12Utpust.mp3"
+        
+        #valve_8_exhale = threading.Thread(target=digitalWrite,args=(8,0,))
+        valve_7_exhale = threading.Thread(target=digitalWrite,args=(7,0,))
+        #motor_3_exhale = threading.Thread(target=digitalWrite,args=(3,0))
+        #motor_2_exhale = threading.Thread(target=digitalWrite,args=(2,1))
         pump_exhale = threading.Thread(target=write_pump,args=(0,))
         sound_exhale = threading.Thread(target=play_sound,args=(breath_path,))
 
-        valve_8_exhale.start()
+        #valve_8_exhale.start()
         valve_7_exhale.start()
+        #motor_3_exhale.start()
+        #motor_2_exhale.start()
         pump_exhale.start()
         sound_exhale.start()
 
-        time.sleep(1)
-        valve_8_exhale.join()
+        time.sleep(2)
+        #valve_8_exhale.join()
         valve_7_exhale.join()
+        #motor_3_exhale.join()
+        #motor_2_exhale.join()
         pump_exhale.join()
         sound_exhale.join()
